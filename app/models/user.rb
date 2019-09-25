85c732c99d50e078b194f8ba41ee75f537f802fa
TerukoMiyaishi <liszt1008@yahoo.co.jp>
2019-09-25 13:34:21 +0000

    Finish editing-users

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true    
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # 渡された文字列のハッシュ値を返します。
   def User.digest(string)
@@ -45,4 +45,6 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+  
+  
 end
